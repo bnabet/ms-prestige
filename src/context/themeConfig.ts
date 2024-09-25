a2f2f59c497f2d82f4fa9cefcbc54e1c3bf18799
bnabet <ben.nabet08@gmail.com>
2024-09-25 16:36:46 +0200

    :sparkles: Create theme

---
+++ src/context/themeConfig.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#1a1a1a",
+    },
+    text: {
+      primary: "#f5f5f5",
+    },
+  },
+});
+
+export default theme;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import theme from "./context/themeConfig.ts";
+
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
